98afdae4e274a86a50c144ad01a79d3f19bff158
Romain Herault <romain.herault@les-tilleuls.coop>
2023-12-05 11:50:14 +0100

    feat: add some new questions

--- questions/questions.ts
+++ questions/questions.ts
@@ -13,7 +13,7 @@ export const fetchQuestions = () => {
       'forms.yaml',
       'http.yaml',
       // 'http-caching.yaml',
-      // 'miscellaneous.yaml',
+      'miscellaneous.yaml',
       'php.yaml',
       'routing.yaml',
       'security.yaml',
